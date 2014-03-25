44d3c00185e8afc5eb8001a4892fdc9cbe6c92e2
James Elías Sigurðarson <james@verdandi.is>
2014-03-25 15:49:14 +0000

    Working on initial KONscript

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ var beautify = require('js-beautify').js_beautify;
 
 var parser   = require('./lib/parser');
 
-module.exports = function (file, beauty, dogeMode) {
-    if (dogeMode) var lines = file.split(/ {3,}|\r?\n/);
+module.exports = function (file, beauty, konMode) {
+    if (konMode) var lines = file.split(/ {3,}|\r?\n/);
     else var lines = file.split(/\r?\n/);
     var script = '';
 
